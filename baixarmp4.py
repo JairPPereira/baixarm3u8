538e7892a52fc57d31d055d2c9abd6bc2aee37d5
JPP <86321461+JairPPereira@users.noreply.github.com>
2025-05-13 07:56:06 -0300

    Update baixarmp4.py

--- baixarmp4.py
+++ baixarmp4.py
@@ -9,6 +9,6 @@ def baixar_video(url, caminho_destino):
         ydl.download([url])
 
 # Exemplo de uso
-url_video = 'https://cdn77-vid.xvideos-cdn.com/9DuS61jNW12Py8tV_RM39Q==,1746293762/videos/hls/1c/b7/38/1cb738fffdec276bebfa2f6358911e75/hls-1080p-a0671.m3u8'
+url_video = 'https://cdn77-vid.xvideos-cdn.com/41HkfGtZEf7EeMWu7Ik9zw==,1747144049/videos/hls/8e/8e/9d/8e8e9da672e976265bdbccc95cfcc3c3/hls-720p.m3u8'
 caminho_destino = 'D:/baixarm3u8/'  # Defina o caminho onde quer salvar o vídeo
 baixar_video(url_video, caminho_destino)
