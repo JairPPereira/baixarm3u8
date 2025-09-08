37019cf20117abca10feee959195a963b483cf12
JPP <86321461+JairPPereira@users.noreply.github.com>
2025-09-08 07:24:00 -0300

    Update baixam3u8.py

--- baixam3u8.py
+++ baixam3u8.py
@@ -2,7 +2,7 @@ import m3u8_To_MP4
 
 if __name__ == '__main__':
     # URL do arquivo M3U8
-    m3u8_url = 'https://b-vz-89851937-53e.tv.pandavideo.com.br/44169e09-1097-43bb-9c45-84499fc08af9/playlist.m3u8'
+    m3u8_url = 'https://ev-h.phncdn.com/hls/videos/202308/04/436805901/720P_4000K_436805901.mp4/master.m3u8?validfrom=1757323012&validto=1757330212&ipa=1&hdl=-1&hash=7ErVNc5GHGIhHqRE6ovrI%2ByyC24%3D'
     
     # Baixar vídeo usando múltiplas threads
     m3u8_To_MP4.multithread_download(m3u8_url)
