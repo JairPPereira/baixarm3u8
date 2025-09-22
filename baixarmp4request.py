26ff1bc8d8312d75d9f2cbb792dba78f4b9f1545
JPP <86321461+JairPPereira@users.noreply.github.com>
2025-09-22 10:37:26 -0300

    Update baixarmp4request.py

--- baixarmp4request.py
+++ baixarmp4request.py
@@ -1,8 +1,8 @@
 import requests
 import sys
 
-url = "http://sinalprivado.info:80/movie/632035/GqGcFV4ntu/884376.mp4"
-nome_arquivo = "884386.mp4"
+url = "http://sinalprivado.info:80/movie/632035/GqGcFV4ntu/849251.mp4"
+nome_arquivo = "849251.mp4"
 
 # Cabeçalhos simulando navegador real
 headers = {
