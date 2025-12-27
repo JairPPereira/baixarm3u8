d6265ae91a06573846722f874a671809b7423e79
JPP <86321461+JairPPereira@users.noreply.github.com>
2025-12-27 08:26:47 -0300

    Update baixam3u8.py

--- baixam3u8.py
+++ baixam3u8.py
@@ -2,7 +2,7 @@ import m3u8_To_MP4
 
 if __name__ == '__main__':
     # URL do arquivo M3U8
-    m3u8_url = 'https://cdn77-vid.xvideos-cdn.com/0MYgkWBm48iU_aLs8WsDKQ==,1766151025/videos/hls/74/14/1d/74141d73a2ebaebbee7aa14b0998eb0e/hls-480p-23718.m3u8'
+    m3u8_url = 'https://cdn77-vid.xvideos-cdn.com/V6UF18WPgCgx1sqisQJIYw==,1766844987/videos/hls/41/57/ab/4157ab7c45a6ca3d0b83b0e20caca39f/hls.m3u8'
     
     # Baixar vídeo usando múltiplas threads
     m3u8_To_MP4.multithread_download(m3u8_url)
